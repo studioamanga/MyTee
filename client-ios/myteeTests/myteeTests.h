1fc2253df35204af5d61105a3ac9e1df74cc4e54
Vincent Tourraine <me@vtourraine.net>
2012-01-28 16:28:08 +0100

    Add new iOS project

---
+++ client-ios/myteeTests/myteeTests.h
@@ -0,0 +1,13 @@
+//
+//  myteeTests.h
+//  myteeTests
+//
+//  Created by Vincent Tourraine on 1/28/12.
+//  Copyright (c) 2012 Keres-Sy, Studio AMANgA. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface myteeTests : SenTestCase
+
+@end
---
+++ client-ios/myteeTests/myteeTests.m
@@ -0,0 +1,32 @@
+//
+//  myteeTests.m
+//  myteeTests
+//
+//  Created by Vincent Tourraine on 1/28/12.
+//  Copyright (c) 2012 Keres-Sy, Studio AMANgA. All rights reserved.
+//
+
+#import "myteeTests.h"
+
+@implementation myteeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in myteeTests");
+}
+
+@end
