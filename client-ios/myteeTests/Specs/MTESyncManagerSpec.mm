6c8bc50e6f9f349edd5a04648ddfb9f5a93e503f
Vincent Tourraine <me@vtourraine.net>
2012-01-31 00:05:08 +0100

    Add sync manager specs

--- client-ios/myteeTests/Specs/MTESyncManagerSpec.mm
+++ client-ios/myteeTests/Specs/MTESyncManagerSpec.mm
@@ -8,6 +8,8 @@
 
 #import "SpecHelper.h"
 
+#import "MTESyncManager.h"
+
 using namespace Cedar::Matchers;
 
 SPEC_BEGIN(MTESyncManagerSpec)
@@ -16,7 +18,10 @@ describe(@"Sync Manager", ^{
     
     it(@"should build ", ^{
         
+        NSURLRequest * request = [MTESyncManager requestForAuthenticatingWithEmail:@"studioamanga@gmail.com" password:@"secret"];
         
+        NSString * expectedURL = @"http://www.studioamanga.com/mytee/api/store/all?login=studioamanga%40gmail.com&password=secret";
+        request.URL.absoluteString should equal(expectedURL);
     }); 
 });
 
