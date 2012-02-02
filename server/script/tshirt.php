dfe6293cfd3bcc3f7c04e12bb4ab1f5676086574
Vincent Tourraine <me@vtourraine.net>
2012-02-02 22:42:22 +0100

    Update /tshirt API to return alternate images if necessary, close #5

--- server/script/tshirt.php
+++ server/script/tshirt.php
@@ -2,9 +2,27 @@
 
 	function clean_tshirt_from_db($tshirt)
 	{
+		$tshirt_alt_image = $tshirt->tsh_img;
+		
 		$tshirt = rename_keys($tshirt, array('tsh_id', 'tsh_name', 'tsh_size', 'tsh_color', 'tsh_condition', 'tsh_rating', 'tsh_tags', 'tsh_store_id', 'tsh_note', 'tsh_img_close'), array('identifier', 'name', 'size', 'color', 'condition', 'rating', 'tags', 'store_id', 'note', 'image_url'), true);
 		
-		$tshirt['image_url'] = 'http://www.studioamanga.com/mytee/img/tshirts/'.$tshirt['identifier'].'/'.$tshirt['image_url'];
+		$img_url_relative = 'img/tshirts/'.$tshirt['identifier'].'/'.$tshirt['image_url'];
+		if(!file_exists('../'.$img_url_relative))
+		{
+			$img_url_relative = str_replace('.jpg', '_300px.jpg', $img_url_relative);
+			
+			if(!file_exists('../'.$img_url_relative))
+			{
+				$img_url_relative = 'img/tshirts/'.$tshirt['identifier'].'/'.$tshirt_alt_image;
+				
+				if(!file_exists('../'.$img_url_relative))
+				{
+					$img_url_relative = str_replace('.jpg', '_300px.jpg', $img_url_relative);
+				}
+			}
+		}
+		
+		$tshirt['image_url'] = 'http://www.studioamanga.com/mytee/'.$img_url_relative;
 		
 		return $tshirt;
 	}
