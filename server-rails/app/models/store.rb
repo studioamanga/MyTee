76012ec923568bfe990cad2b2749e3de94b25c83
Vincent Tourraine <me@vtourraine.net>
2013-04-15 23:53:37 +0200

    Scaffold stores

---
+++ server-rails/app/models/store.rb
@@ -0,0 +1,3 @@
+class Store < ActiveRecord::Base
+  attr_accessible :address, :name, :type
+end
--- server-rails/config/routes.rb
+++ server-rails/config/routes.rb
@@ -1,4 +1,6 @@
 ServerRails::Application.routes.draw do
+  resources :stores
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
